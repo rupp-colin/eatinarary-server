3410c31e3e099bd102e6d6c823544a63d834f8c3
Colin Rupp <colin@thinkful.com>
2018-10-17 09:47:45 -0700

    added recipe model for db

---
+++ models/recipe.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  label: {type: String, required: true},
+  uri: String,
+  image: String,
+  source: String,
+  url: String,
+  healthLabels: String,
+  ingredientLines: Array,
+  instructions: String
+});
+
+//adds 'createdAt' and 'updatedAt' fields
+recipeSchema.set('timestamps', true);
+
+recipeSchema.set('toObject', {
+  virtuals: true, //includes built in virtual 'id'
+  transform: (doc, ret) => {
+    delete ret._id;
+    delete ret.__v;
+  }
+});
