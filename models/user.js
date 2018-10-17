5c813b5ae84fc45fb97257553605d5d0a7a8c57e
Colin Rupp <colin@thinkful.com>
2018-10-17 09:20:05 -0700

    added user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mogoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {type: String, require: true, unique: true},
+  password: {type: String, required: true}
+});
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = password => {
+  return bcrypt.hash(password, 10);
+};
+
+userSchema.set('toObject', {
+  virtuals: true,
+  versionKey: false,
+  transform; (doc, result) => {
+    delete result._id;
+    delete result.__v;
+    delete result.password;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
