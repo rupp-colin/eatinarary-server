c56e4f8cc040ebe76989a14a27a7122d1474901d
Colin Rupp <colin@thinkful.com>
2018-10-18 10:42:20 -0700

    cleaned up bugs between front and backend communication

---
+++ routes/recipes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const Recipe = require('../models/recipe.js');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+router.use(passport.authenticate('jwt', {session: false, failWithError: true}));
+
+
+// ================== GET/READ ALL RECIPES ============= //
+router.get('/', (req, res, next) => {
+  const userId = req.user.id;
+  let filter = { userId: userId };
+
+  Recipe
+    .find(filter)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
+
+// ================== POST/CREATE RECIPE ================= //
+router.post('/', (req, res, next) => {
+  const {label, uri, image, source, url, healthLabels, ingredientLines, instructions} = req.body;
+  const userId = req.user.id;
+
+  if(!label) {
+    const err = new Error('Missing recipe label in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = {label, uri, image, source, url, healthLabels, ingredientLines, instructions, userId};
+
+  Recipe
+    .create(newItem)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
